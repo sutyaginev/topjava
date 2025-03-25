e4e289a5269b1f36223dc104e383d2652284faf3
Evgeniy Sutyagin <sutyaginev@gmail.com>
2025-03-26 00:45:24 +0500

    HW6_0: fix meals

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.web.meal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
@@ -21,11 +22,8 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkIsNew;
 public class MealRestController {
     private static final Logger log = LoggerFactory.getLogger(MealRestController.class);
 
-    private final MealService service;
-
-    public MealRestController(MealService service) {
-        this.service = service;
-    }
+    @Autowired
+    private MealService service;
 
     public Meal get(int id) {
         int userId = SecurityUtil.authUserId();

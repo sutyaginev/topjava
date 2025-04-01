b58f754bab4a006de155456d1a1210c8597ae871
Evgeniy Sutyagin <sutyaginev@gmail.com>
2025-04-01 22:21:51 +0500

    HW7_1: RootControllerTest + ResourceControllerTest

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,7 +2,9 @@ package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.List;
 
@@ -27,6 +29,18 @@ class RootControllerTest extends AbstractControllerTest {
                                 USER_MATCHER.assertMatch(actual, admin, guest, user);
                             }
                         }
+                ));
+    }
+
+    @Test
+    void getMeals() throws Exception {
+        perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals",
+                        MealsUtil.getTos(MealTestData.meals, SecurityUtil.authUserCaloriesPerDay())
                 ));
     }
 }
